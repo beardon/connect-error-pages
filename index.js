be8aa126285433227083c226d20734d70f1e70ef
Aaron Bean <aaron.bean@beardon.com>
2016-10-20 15:40:10 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+function buildPath(statusCode) {
+    var path = require('path');
+    var serverErrorPagesPath = path.join(require('server-error-pages').path, '_site');
+    return path.join(serverErrorPagesPath, statusCode + '-error.html');
+}
+
+module.exports = [
+    function (req, res, next) {
+        return res.sendFile(buildPath(404));
+    },
+    function (err, req, res, next) {
+        // respect err.statusCode
+        if (err.statusCode) {
+            res.statusCode = err.statusCode
+        }
+        // respect err.status
+        if (err.status) {
+            res.statusCode = err.status
+        }
+        // default status code to 500
+        if (res.statusCode < 400) {
+            res.statusCode = 500
+        }
+        return res.sendFile(buildPath(res.statusCode));
+    }
+];
